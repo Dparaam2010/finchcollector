b441ae22b6878cd106ace5e3b7f8f607c401f3e6
Dparaam2010 <dparaam@gmail.com>
2023-09-14 13:15:18 -0700

    add feeding details

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 
 # Create your views here.
-
-finches = [
-  {'name': 'Jasmine', 'breed': 'fatbrown', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Kat', 'breed': 'yellowtail', 'description': 'gentle and loving', 'age': 2},
-]
-
 def home(request):
-    return render(request, 'home.html'),
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def finches_index(request):
-    return render(request, 'finches/index.html', {
-        'finches': finches
-    })
+  cats = Finch.objects.all()
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {
+    'finch': finch
+  })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
