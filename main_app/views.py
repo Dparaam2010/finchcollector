18d2c4e03f7304a5fd25b5db66a25474dcb0142d
Dparaam2010 <dparaam@gmail.com>
2023-09-14 15:18:55 -0700

    part 5 done

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 
 # Create your views here.
@@ -11,20 +12,24 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-  cats = Finch.objects.all()
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {
     'finches': finches
   })
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+  feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
-    'finch': finch
+    'finch': finch, 'feeding_form': feeding_form,
+    'toys': toys_cat_doesnt_have
   })
 
 class FinchCreate(CreateView):
   model = Finch
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
 
 class FinchUpdate(UpdateView):
   model = Finch
@@ -40,6 +45,32 @@ def add_feeding(request, finch_id):
     new_feeding = finch.save(commit=False)
     new_feeding.form_id = finch_id
     new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
+
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
   return redirect('detail', finch_id=finch_id)
   
 
